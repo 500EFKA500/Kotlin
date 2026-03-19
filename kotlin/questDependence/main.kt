09eca90547d16208f3aa18b363713e94ae6a5067
Clinkz <cl@bn>
2026-03-19 12:50:26 +0300

    commit

--- kotlin/questDependence/main.kt
+++ kotlin/questDependence/main.kt
@@ -1,7 +1,7 @@
 package quest_Dependence
 
 
-//////////// ЮРА ЕСЛИ ТЫ УВЕДЕЛ ЭТО КРОМЕ 500EFKA500 ТО КОД СКОПИРОВАЛИ
+
 import de.fabmax.kool.KoolApplication               // Запускает Kool-приложение
 import de.fabmax.kool.PassData
 import de.fabmax.kool.addScene                      // функция - добавить сцену (UI, игровой мир и тд)
@@ -33,8 +33,8 @@ import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.launchIn
+//////////// ЮРА ЕСЛИ ТЫ УВЕДЕЛ ЭТО КРОМЕ 500EFKA500 ТО КОД СКОПИРОВАЛИ
 import kotlinx.coroutines.flow.flatMapLatest
-import lesson11.HudState
 
 // ---------- Зависимости и блокировка квестов ----------- //
 
